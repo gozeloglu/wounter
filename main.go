5c70e516a2e669abd19da0c7d2d0baa32967a481
Gökhan Özeloğlu <gozeloglu@gmail.com>
2023-09-23 16:16:28 +0300

    Count file content with `path` option
    * Counting file content's word number feature added. The option is --path.
    
    * Test case added for new line.
    
    * Count function is updated for newline chars.
    
    * ReadFile helper function implemented for given file directory.
    
    Signed-off-by: Gökhan Özeloğlu <gozeloglu@gmail.com>

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
+
+var filePath = flag.String("path", "", "File path for counting file content")
 
 func main() {
 	flag.Parse()
 
 	s := flag.Args()
+
+	if len(s) != 0 {
+		fmt.Println("\n============================")
+		fmt.Println("Word count:", Counter(s[0]))
+		fmt.Println("============================")
+		return
+	}
+
+	if len(*filePath) == 0 {
+		// TODO Show usage
+		fmt.Println("Invalid file path.")
+		os.Exit(1)
+	}
+
+	content, err := ReadFile(*filePath)
+	if err != nil {
+		fmt.Println("Error while reading file.")
+		os.Exit(1)
+	}
+
 	fmt.Println("\n============================")
-	fmt.Println("Word count:", Counter(s[0]))
+	fmt.Println("Word count:", Counter(content))
 	fmt.Println("============================")
+
 }
